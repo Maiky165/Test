031b389f3f94c6538b33f8d1b2fb28f5f33040d9
Maiky165 <maikyl165@gmail.com>
2025-10-15 23:01:17 +0200

    -scoreCounter counting speed increases with levelSpeed

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -5,14 +5,15 @@ public class ScoreCounter : MonoBehaviour
 {
     public TMP_Text scoreCounterText;
     public GameManager gameManager;
+    public LevelManager levelManager;
 
     public float score;
-    public float scoreMultiplier = 10f;
+    public float scoreMultiplier = 3f;
 
 
     void Update()
     {
-        if (gameManager.gameActive == true) score += Time.deltaTime * scoreMultiplier;     //calculates new score
+        if (gameManager.gameActive == true) score += Time.deltaTime * scoreMultiplier * levelManager.levelSpeed;     //calculates new score
         scoreCounterText.text = score.ToString("0");
     }
 }
